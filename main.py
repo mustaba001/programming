cabc80eb8e257debed8bc4d01fb61949cab4793d
mustaba001 <mahadimustabakuetst@gmail.com>
2025-08-05 09:05:28 +0600

    password tracking
    Here is tracking password from python code

---
+++ main.py
@@ -0,0 +1,20 @@
+from random import *
+
+user_pass = input('Enter your pass : ')
+
+password = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+            'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
+
+guess = ''
+
+while(guess != user_pass):
+    guess = ''
+
+    for letter in range(len(user_pass)):
+        guess_ch = password[randint(0,61)]
+        guess = str(guess) + str(guess_ch)
+    print(guess)
+
+print('your password is : ',guess)
+
+
